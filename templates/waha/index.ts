92bcd19d870656a181e6152c6f5afd00416a324d
Alan Mosko <moskoweb@gmail.com>
2025-09-17 09:51:00 -0300

    wip

--- templates/waha/index.ts
+++ templates/waha/index.ts
@@ -1,4 +1,4 @@
-import { Output, Services } from "~templates-utils";
+import { Output, randomString, Services } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
